ae243c5b304feaa96f6112a235ab375af9b6ecef
Alfredo V <alfredo.villagomez.m@gmail.com>
2019-05-19 14:12:27 -0400

    Playground udpated

--- packages/cabify-components/doczrc.js
+++ packages/cabify-components/doczrc.js
@@ -1,4 +1,11 @@
 export default {
+  title: 'Cabify',
+  themeConfig: {
+    logo: {
+      src: 'https://user-images.githubusercontent.com/4967157/57986184-c3eb0b00-7a3f-11e9-8f47-744007868edf.png',
+      width: 150,
+    },
+  },
   modifyBundlerConfig: (config) => {
     config.resolve.extensions.push('.css');
     config.module.rules.push({
