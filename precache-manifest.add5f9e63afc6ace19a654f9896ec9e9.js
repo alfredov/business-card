ad33b826a2dd7afb0f839fc8d21b937920226680
Alfredo V <alfredo.villagomez.m@gmail.com>
2019-05-19 21:40:29 -0400

    Updates

---
+++ precache-manifest.add5f9e63afc6ace19a654f9896ec9e9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "e387cbe561f4690d4115",
+    "url": "/static/js/13.8ab225c4.chunk.js"
+  },
+  {
+    "revision": "e3b40fd3937790d899eb",
+    "url": "/static/js/0.4a8d242f.chunk.js"
+  },
+  {
+    "revision": "30e2b5801b6d80f6aa9f",
+    "url": "/static/js/2.befb6061.chunk.js"
+  },
+  {
+    "revision": "e7b0911e1df5016b4575",
+    "url": "/static/js/3.1c1abcad.chunk.js"
+  },
+  {
+    "revision": "e363501c5ecd895d6df3",
+    "url": "/static/js/4.be24aac3.chunk.js"
+  },
+  {
+    "revision": "c988e85963723fbf9669",
+    "url": "/static/js/5.ff515fae.chunk.js"
+  },
+  {
+    "revision": "6dba04484fee42b9b841",
+    "url": "/static/js/6.09bbb7e9.chunk.js"
+  },
+  {
+    "revision": "7a5dc0e697a1f354b809",
+    "url": "/static/js/7.61c15f4b.chunk.js"
+  },
+  {
+    "revision": "1b095f599c18f6705b7a",
+    "url": "/static/js/8.87c82c5f.chunk.js"
+  },
+  {
+    "revision": "d975af8837307b401f50",
+    "url": "/static/js/9.91769661.chunk.js"
+  },
+  {
+    "revision": "395f91ea39101325b15a",
+    "url": "/static/js/10.b5f5517a.chunk.js"
+  },
+  {
+    "revision": "dd5a95ecf0f9bd85f4dc",
+    "url": "/static/js/11.5ab34b83.chunk.js"
+  },
+  {
+    "revision": "70ba38715625a9e872a4",
+    "url": "/static/js/12.d9fcabb5.chunk.js"
+  },
+  {
+    "revision": "6a206d6cc4920da0c859",
+    "url": "/static/js/1.4b7ba04f.chunk.js"
+  },
+  {
+    "revision": "f71f41ccbe6079fe0265",
+    "url": "/static/js/14.f8fc280d.chunk.js"
+  },
+  {
+    "revision": "fadac72cb15f754538e87213d762f95c",
+    "url": "/static/media/background.fadac72c.png"
+  },
+  {
+    "revision": "e6d8af15a3cb2e79a9f3",
+    "url": "/static/js/main.de38406c.chunk.js"
+  },
+  {
+    "revision": "cfabbccc8cd5461476ee",
+    "url": "/static/js/runtime~main.2ad0d4ab.js"
+  },
+  {
+    "revision": "379fa8a4243baa976ab4",
+    "url": "/static/js/17.3551fde8.chunk.js"
+  },
+  {
+    "revision": "7aeb50628765319e939d201ccb79fe3a",
+    "url": "/static/media/cabify-logo.7aeb5062.svg"
+  },
+  {
+    "revision": "d767f25e59bed12bcbeef8b2f8ad0634",
+    "url": "/static/media/phone.d767f25e.svg"
+  },
+  {
+    "revision": "ba3fa7b9fc5202922b559ff4e2ce492c",
+    "url": "/static/media/cabify-logo-min.ba3fa7b9.svg"
+  },
+  {
+    "revision": "fc3782a1a12f996c29f943c35d305f65",
+    "url": "/static/media/email.fc3782a1.svg"
+  },
+  {
+    "revision": "1bc83c23d06b2f228a5deb944dec2129",
+    "url": "/static/media/website.1bc83c23.svg"
+  },
+  {
+    "revision": "dd57fd10574ed14a94502bd9ecb2a4bc",
+    "url": "/static/media/address.dd57fd10.svg"
+  },
+  {
+    "revision": "e6d8af15a3cb2e79a9f3",
+    "url": "/static/css/main.2b955c80.chunk.css"
+  },
+  {
+    "revision": "3c3ea6631a6ce93d7fc072709992b9ff",
+    "url": "/index.html"
+  }
+];
