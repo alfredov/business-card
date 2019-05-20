d6223f1360a223608e93fd044af030dedbc0f2e2
Alfredo V <alfredo.villagomez.m@gmail.com>
2019-05-19 21:41:29 -0400

    Updates

---
+++ precache-manifest.d592339acdfb5153c5ff90a549c9fe2b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "e387cbe561f4690d4115",
+    "url": "/business-card/static/js/13.8ab225c4.chunk.js"
+  },
+  {
+    "revision": "e3b40fd3937790d899eb",
+    "url": "/business-card/static/js/0.4a8d242f.chunk.js"
+  },
+  {
+    "revision": "30e2b5801b6d80f6aa9f",
+    "url": "/business-card/static/js/2.befb6061.chunk.js"
+  },
+  {
+    "revision": "e7b0911e1df5016b4575",
+    "url": "/business-card/static/js/3.1c1abcad.chunk.js"
+  },
+  {
+    "revision": "e363501c5ecd895d6df3",
+    "url": "/business-card/static/js/4.be24aac3.chunk.js"
+  },
+  {
+    "revision": "c988e85963723fbf9669",
+    "url": "/business-card/static/js/5.ff515fae.chunk.js"
+  },
+  {
+    "revision": "6dba04484fee42b9b841",
+    "url": "/business-card/static/js/6.09bbb7e9.chunk.js"
+  },
+  {
+    "revision": "7a5dc0e697a1f354b809",
+    "url": "/business-card/static/js/7.61c15f4b.chunk.js"
+  },
+  {
+    "revision": "1b095f599c18f6705b7a",
+    "url": "/business-card/static/js/8.87c82c5f.chunk.js"
+  },
+  {
+    "revision": "d975af8837307b401f50",
+    "url": "/business-card/static/js/9.91769661.chunk.js"
+  },
+  {
+    "revision": "395f91ea39101325b15a",
+    "url": "/business-card/static/js/10.b5f5517a.chunk.js"
+  },
+  {
+    "revision": "dd5a95ecf0f9bd85f4dc",
+    "url": "/business-card/static/js/11.5ab34b83.chunk.js"
+  },
+  {
+    "revision": "70ba38715625a9e872a4",
+    "url": "/business-card/static/js/12.d9fcabb5.chunk.js"
+  },
+  {
+    "revision": "6a206d6cc4920da0c859",
+    "url": "/business-card/static/js/1.4b7ba04f.chunk.js"
+  },
+  {
+    "revision": "f71f41ccbe6079fe0265",
+    "url": "/business-card/static/js/14.f8fc280d.chunk.js"
+  },
+  {
+    "revision": "fadac72cb15f754538e87213d762f95c",
+    "url": "/business-card/static/media/background.fadac72c.png"
+  },
+  {
+    "revision": "6c8332308fd9ae02a08b",
+    "url": "/business-card/static/js/main.69eba486.chunk.js"
+  },
+  {
+    "revision": "b83e12976a9876df2037",
+    "url": "/business-card/static/js/runtime~main.a48e0870.js"
+  },
+  {
+    "revision": "379fa8a4243baa976ab4",
+    "url": "/business-card/static/js/17.3551fde8.chunk.js"
+  },
+  {
+    "revision": "7aeb50628765319e939d201ccb79fe3a",
+    "url": "/business-card/static/media/cabify-logo.7aeb5062.svg"
+  },
+  {
+    "revision": "d767f25e59bed12bcbeef8b2f8ad0634",
+    "url": "/business-card/static/media/phone.d767f25e.svg"
+  },
+  {
+    "revision": "fc3782a1a12f996c29f943c35d305f65",
+    "url": "/business-card/static/media/email.fc3782a1.svg"
+  },
+  {
+    "revision": "ba3fa7b9fc5202922b559ff4e2ce492c",
+    "url": "/business-card/static/media/cabify-logo-min.ba3fa7b9.svg"
+  },
+  {
+    "revision": "1bc83c23d06b2f228a5deb944dec2129",
+    "url": "/business-card/static/media/website.1bc83c23.svg"
+  },
+  {
+    "revision": "dd57fd10574ed14a94502bd9ecb2a4bc",
+    "url": "/business-card/static/media/address.dd57fd10.svg"
+  },
+  {
+    "revision": "6c8332308fd9ae02a08b",
+    "url": "/business-card/static/css/main.fabbb71b.chunk.css"
+  },
+  {
+    "revision": "05119e7d7da3003fdcdb3df16ac88d3e",
+    "url": "/business-card/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.add5f9e63afc6ace19a654f9896ec9e9.js"
+  "/business-card/precache-manifest.d592339acdfb5153c5ff90a549c9fe2b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/business-card/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
